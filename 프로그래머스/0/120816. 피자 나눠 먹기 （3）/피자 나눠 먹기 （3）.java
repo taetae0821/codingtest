35b1c183fdbad74ce65a95c91e3186f4eace92ce
yultae <taeyul0821@gmail.com>
2025-09-11 19:09:43 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.02 ms, Memory: 84 MB -BaekjoonHub

---
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int slice, int n) {
+        int count = 1;
+        while(true){
+            if(slice * count >= n){ 
+                return count;
+            }
+            count++;
+        }
+    }
+}
