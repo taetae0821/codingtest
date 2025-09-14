97ddeefa0b11043f1526795c638bf25abd8a0f16
yultae <taeyul0821@gmail.com>
2025-09-14 11:15:23 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 347.59 ms, Memory: 96.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        
+        int i = 0;
+        for(; i < completion.length; i++) {
+            if (!participant[i].equals(completion[i])) {
+                return participant[i];
+            }
+        }
+        return participant[i]; 
+    }
+}
