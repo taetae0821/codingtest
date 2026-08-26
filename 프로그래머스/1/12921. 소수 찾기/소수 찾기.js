1e15e315431992c5d3b65707d2fd51a8158a92d9
yultae <taeyul0821@gmail.com>
2026-08-26 09:35:08 +0900

    [level 1] Title: 소수 찾기, Time: 34.43 ms, Memory: 58.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -0,0 +1,21 @@
+function solution(n) {
+    
+    const isPrime = new Array(n + 1).fill(true);
+    
+    
+    isPrime[0] = false;
+    isPrime[1] = false;
+    
+    
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (isPrime[i]) {
+          
+            for (let j = i * i; j <= n; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+    
+    
+    return isPrime.filter(value => value === true).length;
+}
